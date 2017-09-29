100d7b950a1028322908659232d61b1e114ed83d
Michael Levy <mike.levy.work@gmail.com>
2017-09-29 13:27:21 -0600

    Comments in YAML, stqrting to work on auto-gen
    gen_code.py is a simple python script that we can hopefully use for
    autogenerating Fortran code (relying on a very simple set of mark-ups to
    indicate where in the template file we want to inject Fortran)

---
+++ gen_code.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# This script reads in
+
+import yaml
+
+# Read YAML file to get variables / values
+with open('parameters.yaml') as parmsfile:
+  parameters = yaml.load(parmsfile)
+
+in_file  = "marbl_settings_mod.template"
+out_file = "marbl_settings_mod.F90"
+
+# Fortran declarations are a dictionary based on datatype in YAML
+types = dict()
+types["string"] = "character(len=char)"
+types["logical"] = "logical(log_kind)"
+types["integer"] = "integer(int_kind)"
+types["real"] = "real(r8)"
+
+
+# Read template file line by line
+with open(in_file) as fin:
+    lines = [x.strip('\n') for x in fin.readlines()]
+
+with open(out_file, 'w') as fout:
+    for single_line in lines:
+        # 1. ignore !!!
+        if single_line.lstrip().startswith('!!!'):
+            continue
+
+        # 2. replace !## with auto-generated code
+        if single_line.lstrip().startswith('!##'):
+            # i. Make sure we keep the leading spaces
+            leading_spaces = ''.ljust(len(single_line) - len(single_line.lstrip()))
+
+            # ii. split single_line into an action and a category
+            line_array = single_line.split()
+            # line_array[0] is "!##"
+            action = line_array[1]
+            cat_name = line_array[2]
+
+            # iii. act based on action
+            if action == "declare":
+                for var_name in parameters[cat_name]:
+                    fout.write("%s%s, target :: %s   ! %s\n" % (leading_spaces,
+                                                                types[parameters[cat_name][var_name]["datatype"]],
+                                                                var_name,
+                                                                parameters[cat_name][var_name]["longname"]))
+            if action == "default":
+                for var_name in parameters[cat_name]:
+                    if parameters[cat_name][var_name]["datatype"] == "string":
+                        fout.write("%s%s = '%s'\n" % (leading_spaces,
+                                                    var_name,
+                                                    parameters[cat_name][var_name]["default_value"]))
+                    if parameters[cat_name][var_name]["datatype"] == "integer":
+                        fout.write("%s%s = %d\n" % (leading_spaces,
+                                                    var_name,
+                                                    parameters[cat_name][var_name]["default_value"]))
+                    if parameters[cat_name][var_name]["datatype"] == "logical":
+                        strval = ".true." if parameters[cat_name][var_name]["default_value"] else ".false."
+                        fout.write("%s%s = %s\n" % (leading_spaces,
+                                                    var_name,
+                                                    strval))
+            continue
+
+        # 3. copy all other lines
+        fout.write("%s\n" % single_line)
