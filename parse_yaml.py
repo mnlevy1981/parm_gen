5daabb64c4e30058c6f740fda8cf254673df68b8
Michael Levy <mike.levy.work@gmail.com>
2017-09-29 15:29:00 -0600

    Some general improvements during a meeting

--- parse_yaml.py
+++ parse_yaml.py
@@ -3,13 +3,23 @@
 import yaml
 
 with open('parameters.yaml') as parmsfile:
-  parameters = yaml.load(parmsfile)
+  parameters = yaml.safe_load(parmsfile)
+
+# Validation
+# ---------
+# 1. Is variable from input file defined in dictionary?
+# 2. Is the value provided valid?
+#    i. datatype match?
+#    ii. YAML could have is_valid() function!
+#
+# NOTE: need to do something about parsing marbl_in
+# (could be here or in the "for var_name" loop)
 
 for cat_name in parameters.keys():
-    print "%s" % cat_name
-    print "----"
+    print "! %s" % cat_name
+    print "! ----"
     category = parameters[cat_name]
-    for var_name in category.keys():
+    for var_name in sorted(category.keys(), key=lambda s: s.lower()):
         variable = category[var_name]
         if variable["datatype"] == "string":
             val = '"%s"' % variable["default_value"]
@@ -17,3 +27,4 @@ for cat_name in parameters.keys():
             val = variable["default_value"]
         print "%s =" % var_name, val
     print ""
+
